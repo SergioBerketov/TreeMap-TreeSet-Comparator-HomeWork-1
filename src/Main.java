32436da55a51e0b950c9d6b33b10f9b218e0a58f
Sergei Berketov <sergio_berketov@mail.ru>
2023-03-25 15:46:53 +0700

    Eraser is added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,27 +10,11 @@ public class Main {
         peoples.add(new Person("Anastasia", "Galickaja-Yamskayja", 8));
         peoples.add(new Person("Leonid", "Dudin", 78));
         peoples.add(new Person("Maxim", "Maximov", 19));
-        peoples.add(new Person("Alister", "Kvicha-Melinkovich-Savich",6));
+        peoples.add(new Person("Alister", "Kvicha-Melinkovich-Savich", 6));
 
-        Comparator<Person> personComparator = (o1, o2) -> {
+        Predicate<Person> isYoungerEighteen = p -> p.age < 18;
 
-            String str1 = o1.surname;
-            String[] surnamesCount1 = str1.split("-");
-
-
-            String str2 = o2.surname;
-            String[] surnamesCount2 = str2.split("-");
-
-            if (surnamesCount1.length > surnamesCount2.length) {
-                return -1;
-            } else if (surnamesCount1.length < surnamesCount2.length) {
-                return 1;
-            } else {
-                return Integer.compare(o2.age, o1.age);
-            }
-        };
-
-        Collections.sort(peoples, personComparator);
+        peoples.removeIf(isYoungerEighteen);
         System.out.println(peoples);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.function.Predicate;
+
 public class Person {
     protected String name;
     protected String surname;
